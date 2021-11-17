96f97ebdc0b45c57cd77d7d1a50823a13661d048
Ann Nunez <anunez2102@gmail.com>
2021-11-17 13:04:00 -0500

    created folder structure

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+// *****CONDITIONALS******
+// Definition: perform differentoperations based on given condition
+// Define whether a number is positive or negative
+
+let integer=-1
+// inside the parenthesis we write a logical statement checking if true/false
+if (integer<0) {
+    console.log ("Negative: " + integer)
+}
